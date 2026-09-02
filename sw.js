a3628c89db74891a677577b3e468c0ca60f5d6b6
m84wy6wftt-lab <m84wy6wftt@privaterelay.appleid.com>
2026-09-02 04:25:10 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='life-v9';const A=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='life-v10';const A=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
